9588806eab39d8b80fac208d398c2f27b853002f
valelectronic <nnanwikevictor@gmail.com>
2025-05-19 22:04:49 +0100

    still testing the functionality

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api"
-      : "https://mellow-gentleness-production.up.railway.app/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
+
 
 export default axiosInstance;
